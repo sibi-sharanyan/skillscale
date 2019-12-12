4a8f673fbf6915606a207660d145b78d0bbc2ee0
sibi sharanyan <sibisharanyanit@gmail.com>
2019-12-12 20:16:07 +0530

    added loading bar for login

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import React , {useState} from "react";
 import axios from 'axios';
 import Cookies from 'js-cookie';
+import Swal from 'sweetalert2';
+import ReactLoading from 'react-loading';
 
 function onSubmit(email , password) {
 
 
   if(email === '' || password === '') return alert('Fill it');
 
+
+  Swal.fire({
+    showConfirmButton: false, 
+    showCloseButton: false,
+    showLoader: true,
+    allowOutsideClick:	false,
+    timerProgressBar: true,
+    imageUrl: process.env.PUBLIC_URL + '/91.gif',
+    width: 500,
+    padding: '5rem',
+
+  });
+
+
+
   axios.post( "http://localhost:8000/users/login", {
  
     email , password
   }).then((res) => {
+
+    Swal.close();
     Cookies.set('token' , res['headers']['x-auth']);
     console.log(res['headers']['x-auth']);
   }).catch((e) => {
-    alert(e);
+
+    console.log(e);
+    Swal.close();
+
+    // alert(e);
   })
 
 }
