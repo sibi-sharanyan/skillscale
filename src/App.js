e914312a9046c57d0444dfaa0e8d840e0433b8b3
sibi sharanyan <sibisharanyanit@gmail.com>
2019-12-12 16:03:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from "react";
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+function onSubmit(email , password) {
+
+
+  if(email === '' || password === '') return alert('Fill it');
+
+  axios.post( "http://localhost:8000/users/login", {
+ 
+    email , password
+  }).then((res) => {
+    Cookies.set('token' , res['headers']['x-auth']);
+    console.log(res['headers']['x-auth']);
+  }).catch((e) => {
+    alert(e);
+  })
+
+}
+
+
+
 
 function App() {
+
+  const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inputbox">
+        <p className="mid-text">Email: </p>
+        <input
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          type="text"
+          
+        />
+      </div>
+
+      <div className="inputbox">
+        <p className="mid-text">Password: </p>
+        <input
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          type="password"
+         
+
+        />
+      </div>
+
+
+<button onClick = { () => onSubmit(email , password)} >Submit</button>
+
     </div>
   );
 }
